7e3c098d699ff04134ffe082a3d7423bf4211ab6
Alex Salomon <alexsalomon.th@gmail.com>
2018-12-10 16:52:59 +0000

    Log all 5xx errors

--- source/server/middlewares.js
+++ source/server/middlewares.js
@@ -12,15 +12,30 @@ module.exports = app => {
   // Set static files to the 'docs' folder in order to render API documentation
   app.use(express.static(path.join(__dirname, '../../docs')))
 
+  // Performance tweak
   app.use(compression())
+
+  // Added security
   app.use(helmet())
-  app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({ extended: true }))
+
+  // Enable CORS
   app.use(cors())
 
+  // Parse JSON and urlenconded responses
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }))
+
+  // Logging HTTP request/response messages
   if (config.env === 'dev') {
     app.use(morgan('dev'))
   } else if (config.env === 'prod' || config.env === 'stag') {
-    app.use(morgan('combined', { stream: { write: message => logger.info(message.trim()) } }))
+    app.use(morgan('combined', {
+      skip(req, res) {
+        return res.statusCode < 500
+      },
+      stream: {
+        write: message => { logger.error(message.trim()) },
+      },
+    }))
   }
 }
