4cfc1cfb06594562d70d51e1ed9ca9d492cdedc1
Alex Salomon <alexsalomon.th@gmail.com>
2018-12-10 16:22:11 +0000

    Create log directory and files upon logger initialization if they don't already exist

--- source/services/logger.js
+++ source/services/logger.js
@@ -1,58 +1,84 @@
+
+const fse = require('fs-extra')
 const winston = require('winston')
 const Sentry = require('winston-sentry-raven-transport')
 const config = require('../config')
 
-let loggerOptions = {}
-switch (config.env) {
-  case 'dev':
-  case 'test':
-    loggerOptions = {
-      transports: [
-        new winston.transports.Console({
-          format: winston.format.simple(),
-          level: 'debug',
-          handleExceptions: true,
-          colorize: true,
-          timestamp: true,
-        }),
-      ],
-    }
-    break
-  case 'prod':
-  default:
-    loggerOptions = {
-      transports: [
-        new winston.transports.File({
-          format: winston.format.json(),
-          filename: `${config.logger.logsPath}error.log`,
-          level: 'error',
-          handleExceptions: true,
-          maxsize: 5242880, // 5MB
-          maxFiles: 5,
-          colorize: false,
-          timestamp: true,
-        }),
-        new winston.transports.File({
-          format: winston.format.json(),
-          level: 'info',
-          filename: `${config.logger.logsPath}combined.log`,
-          handleExceptions: true,
-          maxsize: 5242880, // 5MB
-          maxFiles: 5,
-          colorize: false,
-          timestamp: true,
-        }),
-        new Sentry({
-          dsn: config.logger.sentry.dns,
-          level: config.logger.sentry.level,
-          install: true,
-          config: {
-            captureUnhandledRejections: true,
-          },
-        }),
-      ],
-    }
-    break
+const errorLogFile = `${config.logger.logsPath}error.log`
+const combinedLogFile = `${config.logger.logsPath}combined.log`
+
+async function initialize() {
+  await createLogFiles()
+  const loggerOptions = getLoggerOptions(config.env)
+  return winston.createLogger(loggerOptions)
 }
 
-module.exports = winston.createLogger(loggerOptions)
+async function createLogFiles() {
+  try {
+    await fse.ensureFile(errorLogFile)
+    await fse.ensureFile(combinedLogFile)
+  } catch (err) {
+    throw err
+  }
+}
+
+function getLoggerOptions(env) {
+  let loggerOptions = {}
+
+  switch (env) {
+    case 'dev':
+    case 'test':
+      loggerOptions = {
+        transports: [
+          new winston.transports.Console({
+            format: winston.format.simple(),
+            level: 'debug',
+            handleExceptions: true,
+            colorize: true,
+            timestamp: true,
+          }),
+        ],
+      }
+      break
+    case 'prod':
+    default:
+      loggerOptions = {
+        transports: [
+          new winston.transports.File({
+            format: winston.format.json(),
+            filename: errorLogFile,
+            level: 'error',
+            handleExceptions: true,
+            maxsize: 5242880, // 5MB
+            maxFiles: 5,
+            colorize: false,
+            timestamp: true,
+          }),
+          new winston.transports.File({
+            format: winston.format.json(),
+            level: 'info',
+            filename: combinedLogFile,
+            handleExceptions: true,
+            maxsize: 5242880, // 5MB
+            maxFiles: 5,
+            colorize: false,
+            timestamp: true,
+          }),
+          new Sentry({
+            dsn: config.logger.sentry.dns,
+            level: config.logger.sentry.level,
+            install: true,
+            config: {
+              captureUnhandledRejections: true,
+            },
+          }),
+        ],
+      }
+      break
+  }
+
+  return loggerOptions
+}
+
+
+module.exports = initialize()
